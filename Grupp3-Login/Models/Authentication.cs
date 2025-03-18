d88386baa7650c467a00a573e8fc58238ee352c0
Kirriko95 <adam.karlsson555@hotmail.se>
2025-03-18 12:31:19 +0100

    EN massa skit

---
+++ Grupp3-Login/Models/Authentication.cs
@@ -0,0 +1,17 @@
+﻿namespace Grupp3_Login.Models
+{
+    public class LoginRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+    }
+
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public int UserId { get; set; }
+        public string Role { get; set; }
+    }
+}
+
---
+++ Grupp3-Login/Models/RegisterRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Grupp3_Login.Models
+{
+    public class RegisterRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int RoleId { get; set; }
+        public Role Role { get; set; }
+
+    }
+}
