f6413768f46442e141a79cce576492092e309fbf
Kirriko95 <adam.karlsson555@hotmail.se>
2025-03-18 11:24:59 +0100

    Added AccountServices folder

---
+++ Grupp3-Login/Services/AccountServices.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+
+namespace Grupp3_Login.Services
+{
+    public class AccountService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiBaseUrl;
+
+        public AccountService(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _apiBaseUrl = configuration["ApiBaseUrl"];
+        }
+
+        public async Task<bool> RegisterCustomerAsync(RegisterRequest request)
+        {
+            var response = await _httpClient.PostAsJsonAsync($"{_apiBaseUrl}/account/register", request);
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
